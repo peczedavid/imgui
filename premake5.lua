026965279e33d82557afe6da7c0d8e66af543311
peczedavid <peczedavid@gmail.com>
2022-06-22 20:58:36 +0200

    Added tables.cpp

--- premake5.lua
+++ premake5.lua
@@ -9,15 +9,16 @@ project "ImGui"
 	files
 	{
 		"imconfig.h",
-    "imgui.h",
-    "imgui.cpp",
-    "imgui_draw.cpp",
-    "imgui_internal.h",
-    "imgui_widgets.cpp",
-    "imgui_rectpack.h",
-    "imgui_textedit.h",
-    "imgui_truetype.h",
-    "imgui_demo.cpp"
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_tables.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imgui_rectpack.h",
+		"imgui_textedit.h",
+		"imgui_truetype.h",
+		"imgui_demo.cpp"
 	}
 
 	filter "system:windows"
